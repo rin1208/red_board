f03d5b7eb8905204e4008cec3985252e23c57193
MokeeeMokeee <moke.0922.moke@gmail.com>
2019-12-25 00:56:18 +0900

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,36 @@
 <template>
   <div>
     <div id="app">
-      <h2>Xmasぼっち掲示板</h2>
+      <h2 class="chatTitle">
+        <span class="chatTitleStyle">Xmas</span>ぼっち掲示板
+      </h2>
       <hr />
-      <div style="margin-bottom: 1.1em;" v-for="(text, index) in texts" :key="index">
+      <div
+        style="margin-bottom: 1.1em;"
+        v-for="(text, index) in texts"
+        :key="index"
+      >
         <p>
-          <span class="text"
-            >名前: {{ text[0] }}「{{ text[1] }} - {{ text[2] }}」</span
-          >
-          <button @click="remove(n)">Remove</button>
+          {{ index + 1 }} 名前: <span class="chatName">{{ text[0] }}</span> -
+          {{ text[2] }}
         </p>
+        <p class="chatText">{{ text[1] }}</p>
       </div>
       <hr />
       <div>
-        <label>名前：</label>
-        <input v-model="newName" />
-        <br />
-        <label>ひとこと：</label>
-        <input v-model="newComment" />
-        <br />
-        <button style="margin-top: 1.5em;" @click="add">送信</button>
+        <div class="chatAlertBox">
+          <p>名前は20文字以下でお願いします</p>
+          <p>メッセージは200文字いないでお願いします</p>
+        </div>
+        <div class="chatFlex">
+          <label>名前</label>
+          <input v-model="newName" maxlength="20" style="width: 9rem;"/>
+        </div>
+        <div class="chatFlex">
+          <label>メッセージ</label>
+          <textarea v-model="newComment" :rows="rows" maxlength="400" />
+        </div>
+        <button @click="add">送信</button>
       </div>
     </div>
   </div>
@@ -37,15 +48,22 @@ export default {
       newComment: null
     };
   },
-  mounted() {
-    if (localStorage.getItem("texts")) {
-      try {
-        this.texts = JSON.parse(localStorage.getItem("texts"));
-      } catch (e) {
-        localStorage.removeItem("texts");
-      }
+  async asyncData({ app }){
+    const response = await app.$axios.$get("/request");
+    return{
+      texts: response.items
     }
   },
+  // localstrageを使うならこれ使う
+  // mounted() {
+  //   if (localStorage.getItem("texts")) {
+  //     try {
+  //       this.texts = JSON.parse(localStorage.getItem("texts"));
+  //     } catch (e) {
+  //       localStorage.removeItem("texts");
+  //     }
+  //   }
+  // },
   methods: {
     add() {
       if (!this.newName) this.newName = "匿名希望";
@@ -62,10 +80,87 @@ export default {
       this.save();
     },
     save() {
-      console.log(this.texts)
+      console.log(this.texts);
       let parsed = JSON.stringify(this.texts);
       localStorage.setItem("texts", parsed);
+    }
+  },
+  computed: {
+    rows() {
+      if (this.newComment) {
+        let num = this.newComment.split("\n").length;
+        return num > 4 ? num : 4;
+      }
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.chat {
+  &Flex {
+    display: flex;
+    flex-direction: column;
+  }
+  &Title {
+    font-family: fantasy;
+    text-align: center;
+    &Style {
+      color: #dd3e54;
+      background: -webkit-linear-gradient(0deg, #dd3e54, #6be585);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+  }
+  &Text {
+    padding-left: 1rem;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+  }
+  &Name {
+    font-weight: bold;
+  }
+  &Alert {
+    &Box {
+      background-color: #990000;
+      padding: 1rem;
+      margin: 2rem 0;
+      display: inline-block;
+      /deep/ p {
+        color: #fff;
+        font-weight: bold;
+      }
+    }
+  }
+}
+
+hr {
+  border: none;
+  border-top: dashed 1px #ff0000;
+  height: 1px;
+  color: #ffffff;
+  margin: 1rem 0;
+}
+
+button {
+  display: inline-block;
+  margin: 1.5rem 0 0 1.5rem;
+  border-radius: 5%; /* 角丸       */
+  font-size: 18pt; /* 文字サイズ */
+  text-align: center; /* 文字位置   */
+  cursor: pointer; /* カーソル   */
+  padding: 12px 12px; /* 余白       */
+  background: #990000; /* 背景色     */
+  color: #ffffff; /* 文字色     */
+  line-height: 1em; /* 1行の高さ  */
+  transition: 0.3s; /* なめらか変化 */
+  box-shadow: 6px 6px 3px #666666; /* 影の設定 */
+  border: 2px solid #990000; /* 枠の指定 */
+}
+
+button:hover {
+  box-shadow: none; /* カーソル時の影消去 */
+  color: #990000; /* 背景色     */
+  background: #ffffff; /* 文字色     */
+}
+</style>
