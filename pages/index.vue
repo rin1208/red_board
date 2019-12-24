8cc23c865d7a2b2c741eb5aef700f7f729354f03
MokeeeMokeee <moke.0922.moke@gmail.com>
2019-12-24 22:44:20 +0900

    setup ok

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,33 @@
 <template>
   <div>
-    <div class="container mx-auto" id="app">
-      <h2 class="text-3xl">ひと（り）ごと掲示板</h2>
+    <div id="app">
+      <h2>Xmasぼっち掲示板</h2>
       <hr />
-      <div style="margin-bottom: 1.1em;" v-for="(text,n) in texts" :key="text">
+      <div style="margin-bottom: 1.1em;" v-for="(text, n) in texts" :key="text">
         <p>
-          <span class="text">名前: {{text[0]}}「{{text[1]}} - {{text[2]}}」</span>
-          <button
-            class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-            @click="remove(n)"
-          >Remove</button>
+          <span class="text"
+            >名前: {{ text[0] }}「{{ text[1] }} - {{ text[2] }}」</span
+          >
+          <button @click="remove(n)">Remove</button>
         </p>
       </div>
-
       <hr />
       <div>
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">名前：</label>
-        <input
-          class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-          v-model="newName"
-        />
+        <label>名前：</label>
+        <input v-model="newName" />
         <br />
-        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">ひとこと：</label>
-        <input
-          class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-          v-model="newComment"
-        />
+        <label>ひとこと：</label>
+        <input v-model="newComment" />
         <br />
-        <button
-          style="margin-top: 1.5em;"
-          class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-          @click="add"
-        >送信</button>
+        <button style="margin-top: 1.5em;" @click="add">送信</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import dayjs from 'dayjs';
-import 'dayjs/locale/ja';
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
 
 export default {
   data() {
